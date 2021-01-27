b7a548a2a9755fe6e01185b003a0d9dca7cf2f4c
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-27 07:32:11 +0000

    commented previously added code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   console.log("DOM loaded and ready");
   $('#tweet-text').on('input', function() {
+    // variables used to store the number of characters in textarea
+    // and a reference to text in counter class
     let differential = this.textLength;
     let $outputValue = $('#tweet-text').siblings().last().children().last().html();
+    // changes color of counter when below zero
     if ($outputValue < 0) {
       $('.counter').addClass("red-text");
     } else if ($outputValue > 0) {
       $('.counter').removeClass("red-text");
     }
+    // updates value to user how many more characters they allowed to input
     $('#tweet-text').siblings().last().children().last().html(140 - differential);
   });
 });
