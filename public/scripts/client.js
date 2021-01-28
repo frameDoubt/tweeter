39852f08165c4f2a4385d1a47a65c7f6b3682c75
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-28 07:43:08 +0000

    implemented 2 functions in the client to buildthe DOM dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const moment = require('moment');
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
 
 const createTweetElement = function(data) {
   return `
   <article>
   <header>
     <div>
-      <img src="${data.user.avatars}" alt="" />
+      <img class="avatar" src="${data.user.avatars}" alt="" />
       <p>${data.user.name}</p>
     </div>
     <p id="tweeterHandle">${data.user.handle}</p>
   </header>
   <p class="tweetArea">${data.content.text}</p>
   <footer>
-    <p id="daysSinceTweet">${moment(data.created_at)}</p>
+    <p id="daysSinceTweet">${moment(data.created_at.toString(), 'x').fromNow()}</p>
     <p id="icons">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -40,5 +52,11 @@ const createTweetElement = function(data) {
 </article>
   `;
 };
-const $tweet = createTweetElement(tweetData);
-$('#tweetsGallery').append($tweet);
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet, `<br>`));
+  }
+};
+
+renderTweets(data);
